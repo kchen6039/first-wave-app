e7b8e2c61028ced52085f012406a262dedc8b37c
kchen6039 <kchen6039@gmail.com>
2019-11-19 20:32:45 +0800

    Added view and template for detailed blog post as well as CSS for the site.

--- report/views.py
+++ report/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Report
 
@@ -7,3 +7,7 @@ from .models import Report
 def post_list(request):
     reports = Report.objects.filter(started_date__lte=timezone.now()).order_by('started_date')
     return render(request, 'report/post_list.html', {'reports': reports})
+
+def post_detail(request, pk):
+    report = get_object_or_404(Report, pk=pk)
+    return render(request, 'report/post_detail.html', {'report': report})
